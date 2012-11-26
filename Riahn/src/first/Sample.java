37f691fe21fed81077bca5b22f6dd924fcf53678
Riahn.Lim <riahn@ahems.co.kr>
2012-11-26 12:28:00 +0900

    Update index.html

---
+++ Riahn/src/first/Sample.java
@@ -0,0 +1,15 @@
+package first;
+
+public class Sample {
+
+	/**
+	- 클래스 이름은 메인 함수가 있는 클래스의 이름과 같아야 한다.
+	- 
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Sample !!");
+	}
+
+}
---
+++ Riahn/src/myself/Using.java
@@ -0,0 +1,19 @@
+package myself;
+
+public class Using {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//유상
+		//010-8295-1070
+		//ryutan07@nate.com
+		System.out.print("123");
+		
+
+	}
+
+}
